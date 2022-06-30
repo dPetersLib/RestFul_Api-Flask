e2db9729229c9ab7ec8d04a56d937034b53e9447
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-06-30 18:47:29 +0100

    Hot Fix updated the connection_url. For further works on this project, I suggest the connection parameters should be stored on the local machine.

--- api/__init__.py
+++ api/__init__.py
@@ -8,13 +8,13 @@ jwt = JWTManager(app)
 app.config['JWT_SECRET_KEY'] = 'Your_Secret_Key'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 
-connection_url = 'mongodb+srv://peter:peter@python-cluster.wfuaylv.mongodb.net/?retryWrites=true&w=majority'
+connection_url = 'mongodb+srv://sloovi:sloovi@template.t6v0y.mongodb.net/?retryWrites=true&w=majority'
 client = MongoClient(connection_url)
 
 # Database
-Database = client.get_database('myfirstdb')
+Database = client.get_database('template')
 # Table
-users_collection = Database.users
+users_collection = Database.Users
 template_collection = Database.templates
 
 
